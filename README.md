ac85f1a2ffa5b8dd56c701cd478c8d85e249bdc4
Rita HC <ritahameerchauhan@gmail.com>
2023-01-17 11:53:01 -0500

    Commit

--- README.md
+++ README.md
@@ -8,3 +8,17 @@
 - (Maybe gather data from a 3rd party API)
 
 This app will start as an API, that receives requests and sendd JSON response, but eventually we will add a view layer that will render html in our browser
+
+This an MVC application
+We're using the MVC system for organizing our code.
+This breaks down our code into 3 parts
+MVC stands for 
+-Models
+-Views
+-Controllers
+
+Models- All of our data, what shape it's in and what resource we're using(models), and how our resources relate to one another.
+
+Views- All the different ways we can see our data, whether it's as a JSON response, or an actual HTML response, this determines how our data can be viewed by others.
+
+Controllers - 
--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-////////////////////////////////
-//Import Dependencies         //  
-////////////////////////////////
+/////////////////////////////////////
+//// Import Dependencies         ////
+/////////////////////////////////////
 const express = require('express') // import the express framework
-const mongoose = require('mongoose') // import the express mongoose
-const morgan = require('morgan') // import the express morgan request logger
-require('dotenv').config() // load my ENV file's variable
+const mongoose = require('mongoose') // import the mongoose library
+const morgan = require('morgan') // import the morgan request logger
+require('dotenv').config() // Load my ENV file's variables
+const path = require('path')
 
-////////////////////////////////
-//Database Connection         //  
-////////////////////////////////
-//This is where we will set our inputes for a database connect function
+/////////////////////////////////////
+//// Import Models               ////
+/////////////////////////////////////
+const Fruit = require('./models/fruits')
+
+/////////////////////////////////////
+//// Database Connection         ////
+/////////////////////////////////////
+// this is where we will set up our inputs for our database connect function
 const DATABASE_URL = process.env.DATABASE_URL
 // here is our DB config object
 const CONFIG = {
@@ -21,25 +27,57 @@ const CONFIG = {
 mongoose.connect(DATABASE_URL, CONFIG)
 
 // Tell mongoose what to do with certain events
-// what happens when we open, disonnect, or get an error
-
+// what happens when we open, diconnect, or get an error
 mongoose.connection
-    .on('open', ()=> console.log('Connected to Mongoose'))
-    .on('close', ()=> console.log('Disconnected to Mongoose'))
-    .on('error', ()=> console.log('An error occured: \n', err))
+    .on('open', () => console.log('Connected to Mongoose'))
+    .on('close', () => console.log('Disconnected from Mongoose'))
+    .on('error', (err) => console.log('An error occurred: \n', err))
 
 
-/////////////////////////////////////////
-//Create and Express App Object        //  
-/////////////////////////////////////////
-
+/////////////////////////////////////
+//// Create our Express App Object //
+/////////////////////////////////////
 const app = express()
 
+/////////////////////////////////////
+//// Middle Ware                 ////
+/////////////////////////////////////
+app.use(morgan('tiny'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('pubmic'))
+app.use(express.json())
 
-/////////////////////////////////////////
-//Server listener                      //  
-/////////////////////////////////////////
+
+/////////////////////////////////////
+//// Routes                      ////
+/////////////////////////////////////
+app.get('/', (req, res) => {
+    res.send('Server is live, ready for requests')
+})
+
+app.get('/fruits/seed', (req,res)=> {
+    const startFruits = [
+        {name: 'Orange', color: 'orange', readyToEat: true},
+        {name: 'Grape', color:'purple' , readyToEat: true},
+        {name: 'Banana', color: 'green', readyToEat: false},
+        {name: 'Strawberry', color: 'red', readyToEat: false},
+        {name: 'Coconut', color: 'brown', readyToEat: true }
+    ]
+
+   Fruit.deleteMany({})
+    .then(()=> {
+        Fruit.create(startFruits)
+            .then(data=> {
+                res.json(data)
+            })
+            .catch(err=> console.log(`The following error occured \n`, err ))
+    })
+})
+
+/////////////////////////////////////
+//// Server Listener             ////
+/////////////////////////////////////
 const PORT = process.env.PORT
-app.listen(PORT, ()=> console.log(`Now listening to the sweet sounds of port: ${PORT}`))
+app.listen(PORT, () => console.log(`Now listening to the sweet sounds of port: ${PORT}`))
 
-//End
+// END
